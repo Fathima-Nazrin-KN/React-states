1ae0866f28f23faeb841b2bc679a9344969638e6
althaf2 <althafnazeernazeer@gmail.com>
2023-09-20 20:00:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const [name,setName] = useState();
+  const [names,setNames] = useState([]);
+
+  function handleChange(event){
+      setName(event.target.value);
+  }
+  function addData(){
+    setNames([...names,name])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>{name}</h1>
+      <input onChange={handleChange}/>
+     { names.map(
+      (n)=><h2>{n}</h2>
+      )}
+      <button onClick={addData}>submit</button>
     </div>
   );
 }
